299c20167d387ee39fee9c64edb0a6883debbc36
rex.houy@gmail.com <rex.houy@gmail.com>
2015-10-14 11:46:34 +0800

    back button

--- www/js/index.js
+++ www/js/index.js
@@ -1,37 +1,10 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-            app.receivedEvent('deviceready');
-            document.addEventListener("backbutton", function(e){
-                    navigator.app.exitApp();
-            }, false);
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady(){
+        document.addEventListener("backbutton", function(e){
+                e.preventDefault();
+                navigator.app.exitApp();
+        }, false);
+}
 
 var medicare = angular.module('Medicare', [
         'ngAnimate',
@@ -102,4 +75,3 @@ var medicare = angular.module('Medicare', [
                         redirectTo: "/"
                 });
 });
-app.initialize();
